af422d3ac8eaad0774033324888b58b4145ce7e1
Antonina Yazykova <adrianery94@>
2020-11-02 13:56:54 +0300

    java commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        float cost = 1700;
+        float bonus;
+        if (cost > 1000) {
+            bonus = cost / 100;
+            System.out.println(bonus);
+
+        }
+    }
+}
+
